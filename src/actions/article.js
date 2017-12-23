88c7e76d5438296f85daa65e3743d3c993275d09
arpithapatil <arpitha.patil@outlook.com>
2017-12-23 19:39:41 +0000

    initial tests passed for articles and test reducer

--- src/actions/article.js
+++ src/actions/article.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import * as types from './types';
+import * as types from '../types';
 
 import {API_URL} from '../../config';
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import {combineReducers} from 'redux';
 
-import test from './test';
+import test from './testReducer';
 import articles from './articleReducer';
 
 const reducer = combineReducers({
