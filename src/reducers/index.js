c3b23b4cfe5ff82132e5ccfe7f523a8f838d3f31
arpithapatil <arpitha.patil@outlook.com>
2017-12-25 09:44:57 +0000

    added userReducer in combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,9 +4,10 @@ import test from './testReducer';
 import articles from './articleReducer';
 import comments from './commentReducer';
 import topics from './topicReducer';
+import users from './userReducer';
 
 const reducer = combineReducers({
-  test, articles, comments, topics
+  test, articles, comments, topics, users
 });
 
 export default reducer;
