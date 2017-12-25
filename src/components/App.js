1dd4ce750aa86f84ad9490e597ff5440ff131b13
arpithapatil <arpitha.patil@outlook.com>
2017-12-25 17:28:26 +0000

    rendered articles on homepage

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom'; 
 import Homepage from './HomePage';
+import ArticleList from './ArticleList';
+import ArticleItem from './ArticleItem';
+import Comments from './Comments';
 import NoMatch from './NoMatch';
+import './App.css';
+
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   render () {
     return (
       <BrowserRouter>
@@ -11,6 +19,10 @@ class App extends React.Component {
           <div>
             <Switch>
               <Route exact path='/' component= {Homepage}/>
+              <Route path='/topics/:topic/articles' component= {ArticleList}/>
+              <Route path='/articles' component= {ArticleList}/>
+              <Route path='/articles/:article' component= {ArticleItem}/>
+              <Route path='/articles/:article/comments' component= {Comments}/>
               <Route component= {NoMatch}/>
             </Switch>
           </div>
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,13 +1,60 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import PT from 'prop-types';
+import {fetchArticles} from '../actions/article';
 
 class Homepage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.props.fetchArticles();
+  }
+
   render () {
     return (
       <div>
         <h1>Northcoders News</h1>
+        {/* <h3>Most Popular Stories</h3> */}
+        <div className='all-articles'>
+          <h1>Home</h1>
+         
+          {this.props.articles.map((article) => {
+             
+            const topic = article.belongs_to;
+            
+            return (
+              <div key={article.title}>
+                <p>{article.title}</p>
+                <p>{topic}</p>
+                <p>{article.votes}</p>
+                <p>{article.comments}</p>
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
 }
 
-export default Homepage;
+const mapStateToProps = state => ({
+  articles: state.articles.data,
+  loading: state.articles.loading,
+  error: state.articles.error
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchArticles: () => {
+    dispatch(fetchArticles());
+  }
+});
+
+Homepage.propTypes = {
+  articles: PT.array.isRequired,
+  error: PT.any,
+  fetchArticles: PT.func.isRequired
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Homepage);
