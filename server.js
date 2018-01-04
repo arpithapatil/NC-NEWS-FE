927d659cbfb27ca8e266aa7c5de5da831180deca
arpithapatil <arpitha.patil@outlook.com>
2018-01-04 12:30:13 +0000

    modified package.json

--- server.js
+++ server.js
@@ -5,4 +5,6 @@ app.use(express.static('public'));
 app.get('*', (req,  res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
+
 app.listen(process.env.PORT || 8080);
+console.log('server is running');
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {getMostPopular} from '../actions/article';
 import {NavLink} from 'react-router-dom';
+import Loading from './Load.js';
 import PT from 'prop-types';
 
 export class HomePage extends React.Component {
@@ -14,43 +15,44 @@ export class HomePage extends React.Component {
 
   render () {
     return (
-      <div className='main container-fluid'>
-        <div className='appear-articles'>
-          <h2 className='mostpopular'>Most Popular Stories</h2>
-          <div className='row'>
-            {this.props.articles.map((article, i) => {
-              const title = article.title.split(' ').map((word) => {
-                if (word.toLowerCase().match(/[aeiou]/)) {
-                  return word[0].toUpperCase() + word.slice(1).toLowerCase();
-                }
-                else return word.toUpperCase();
-              }).join(' ');
-              while (i < 15) {
-                return (
-                  <div key={article.title} className=' col-xs-8 col-md-offset-6 articles '>
-                    <div className='row'>
-                      <div className='col-md-2'>
-                        {/* <img className='arrowup' src='https://www.iconexperience.com/_img/o_collection_png/green_dark_grey/256x256/plain/arrow_up.png' alt='votes' /> */}
-                        <p className='num'>{article.votes} votes</p>
-                        {/* <img className='arrowdown' src='https://www.iconexperience.com/_img/o_collection_png/green_dark_grey/512x512/plain/arrow_down.png' alt='votes' /> */}
-                      </div>
-                      <div className='col-md-8 '>
-                        <h3><NavLink to={`/articles/${article._id}`} className='article-title-box'>{title}<br /></NavLink></h3>
-                      </div>
-                      <div className='col-md-2'>
-                        <p><NavLink className='comment-link' commentflag='true' to={`/articles/${article._id}/comment`}>{article.comments}<br />comments</NavLink></p>
+      this.props.loading ? <Loading/> :
+        <div className='main container-fluid'>
+          <div className='appear-articles'>
+            <h2 className='mostpopular'>Most Popular Stories</h2>
+            <div className='row'>
+              {this.props.articles.map((article, i) => {
+                const title = article.title.split(' ').map((word) => {
+                  if (word.toLowerCase().match(/[aeiou]/)) {
+                    return word[0].toUpperCase() + word.slice(1).toLowerCase();
+                  }
+                  else return word.toUpperCase();
+                }).join(' ');
+                while (i < 15) {
+                  return (
+                    <div key={article.title} className=' col-xs-8 col-md-offset-6 articles '>
+                      <div className='row'>
+                        <div className='col-md-2'>
+                          {/* <img className='arrowup' src='https://www.iconexperience.com/_img/o_collection_png/green_dark_grey/256x256/plain/arrow_up.png' alt='votes' /> */}
+                          <p className='num'>{article.votes} votes</p>
+                          {/* <img className='arrowdown' src='https://www.iconexperience.com/_img/o_collection_png/green_dark_grey/512x512/plain/arrow_down.png' alt='votes' /> */}
+                        </div>
+                        <div className='col-md-8 '>
+                          <h3><NavLink to={`/articles/${article._id}`} className='article-title-box'>{title}<br /></NavLink></h3>
+                        </div>
+                        <div className='col-md-2'>
+                          <p><NavLink className='comment-link' commentflag='true' to={`/articles/${article._id}/comment`}>{article.comments}<br />comments</NavLink></p>
+                        </div>
                       </div>
                     </div>
-                  </div>
 
-                );
+                  );
+                }
               }
-            }
 
-            )}
+              )}
+            </div>
           </div>
         </div>
-      </div>
     );
   }
 }
