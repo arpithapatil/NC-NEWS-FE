76332b8f163474c4ef5189299c11b2a36ff75ea2
arpithapatil <arpitha.patil@outlook.com>
2018-01-04 17:48:12 +0000

    made changes to package.json

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import store from './store';
-if (process.env.NODE_ENV !== 'production') {
-  console.log('Looks like we are in development mode!');
-}
 
 ReactDOM.render(
   <Provider store={store}>
